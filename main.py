c609a6fa2832b70f8a5ef95d205723c45adf7b07
Jack Pre <89611672+prevattj21@users.noreply.github.com>
2022-04-24 00:37:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -15,3 +15,29 @@ test_budget.set_balance(0)
 test_budget.set_category("New category")
 print("Test budget category:", test_budget.get_category())
 print("Test budget balance:", test_budget.get_balance())
+
+
+#Jack Stuff So Far
+from Budget import Budget
+
+def category_budget():
+        categories = []
+        category = input("Please select a category: ")
+
+        while category not in categories:
+            print("Category", category, "is not in category list. Would you like to create a new category for", category,
+                  "?\nY or N")
+            new_cat = input()
+
+            if new_cat == "Y":
+                categories.append(category)
+
+                budget = float(input("Please enter your budget: "))
+
+                Budget(category, budget)
+                return
+
+            else:
+                break
+
+category_budget()
