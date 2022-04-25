334bde7f8754f27672d86050a8d0f28ece806add
AdriannaCT <89425990+AdriannaCT@users.noreply.github.com>
2022-04-24 22:05:37 -0400

    Update main.py

--- main.py
+++ main.py
@@ -17,17 +17,25 @@ print("Test budget category:", test_budget.get_category())
 print("Test budget balance:", test_budget.get_balance())
 
 
-
+#Jack Stuff So Far
 from Budget import Budget
+from Transaction_History import Transactions, Node
 
 categories = []
-category = input("Please select a category or press Enter to quit: ")
+category = input("Please enter a category, type 'transactions' to get full list of transactions, or press Enter to quit:")
+transaction_list = Transactions()
 
 while len(category) > 0:
-    if category not in categories:
+    if category not in categories and category is not "transactions":
         categories.append(category)
 
         budget = float(input("Please enter your budget: "))
+    elif category is "transactions":
+        node = transaction_list.head
+        while node is not None:
+            print("Category: {}, Operation: {}, Amount: {}".format(node.category, node.action, node.amount))
+            node = node.next
+        print()
     else:
 
         final = Budget(category, budget)
@@ -42,12 +50,17 @@ while len(category) > 0:
             final.deposit(dep_amount)
             print("New", category, "budget balance:", final.get_balance())
             budget = final.get_balance()
+            new_node = Node(category, operation, dep_amount)
+            transaction_list.append(new_node)
+
 
         elif operation == "Withdraw":
             with_amount = float(input("Enter withdraw amount: "))
             final.withdraw(with_amount)
             print("New", category, "budget balance:", final.get_balance())
             budget = final.get_balance()
+            new_node = Node(category, operation, with_amount)
+            transaction_list.append(new_node)
 
         category = input("\nPlease select a category or press Enter to quit: ")
 else:
